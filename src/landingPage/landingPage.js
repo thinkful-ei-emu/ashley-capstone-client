ef82a6fd943228aba027419c1dc3104535c21bbb
Ashley Oelbaum <ashleyoelbaum@gmail.com>
2019-08-16 10:36:16 -0400

    fixed styling and added iewport

--- src/landingPage/landingPage.js
+++ src/landingPage/landingPage.js
@@ -13,18 +13,18 @@ class LandingPage extends React.Component {
     return (
       <div className="landing-page"> 
       
-        <section>        
+        <div>        
             <h2>Create your own masterpiece</h2>
         
        
         <p>L'Artiste allows for you to create your own artwork and display it in a gallery of your choice. Just like an artist who runs an exhibition, you can create a variety of art pieces that are organized by galleries of different themes!</p>
-      </section>
-      <section>      
+      </div>
+      <div>      
             <h2>Create galleries to display your artwork</h2>      
       
         <p>Each time you create your piece of art, you can save it to a gallery of your choice. You can view a list of your galleries by navigating to your profile page</p>
-      </section>   
-      <section className="bottom-section">
+      </div>   
+      <div className="bottom-section">
       <div className="landing-image-container">
       <img className="landing-image" src={LandingImage} alt="art sample"/>          
         </div>
@@ -32,7 +32,7 @@ class LandingPage extends React.Component {
         <NavLink to="/register" className="link-size">Register</NavLink>  
         <h2>Already a member?</h2>
         <NavLink to="/login" className="link-size">Login</NavLink>    
-      </section>
+      </div>
     
       </div>          
        

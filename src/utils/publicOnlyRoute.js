ef58bd812be037f83b40f5349a12c6de9507c499
Ashley Oelbaum <ashleyoelbaum@gmail.com>
2019-08-19 15:28:03 -0400

    merged with master

--- src/utils/publicOnlyRoute.js
+++ src/utils/publicOnlyRoute.js
@@ -4,14 +4,20 @@ import TokenService from '../services/tokenService'
 
 export default function PublicOnlyRoute({ render, ...props }) {
   const Render = render
+  // console.log( TokenService.hasAuthToken());
+  // console.log(props)
   return (
+ 
     <Route
       {...props}
+     
       render={componentProps => (
+       
         TokenService.hasAuthToken()
           ? <Redirect to={'/'} />
           : <Render {...componentProps} />
       )}
     />
+  
   )
 }

423ac44e260a579994357b90ff1fcd2d2618aca7
Ashley Oelbaum <ashleyoelbaum@gmail.com>
2019-08-22 17:03:41 -0400

    merged with master

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export default {
-  API_ENDPOINT: 'https://obscure-journey-16541.herokuapp.com/api',
-  TOKEN_KEY: process.env.TOKEN_KEY || 'auth'
+  // API_ENDPOINT: 'https://obscure-journey-16541.herokuapp.com/api',
+  TOKEN_KEY: process.env.TOKEN_KEY || 'auth',
+  API_ENDPOINT: 'http://localhost:8000/api',
   
 }
 

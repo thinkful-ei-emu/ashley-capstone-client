191d2a46328b764b6f00ddd017bffec4cc967fae
Ashley Oelbaum <ashleyoelbaum@gmail.com>
2019-08-22 22:33:33 -0400

    merged with newFeature to incorporate login and register styling'

--- src/config.js
+++ src/config.js
@@ -1,7 +1,4 @@
 export default {
-  // API_ENDPOINT: 'https://obscure-journey-16541.herokuapp.com/api',
-  TOKEN_KEY: process.env.TOKEN_KEY || 'auth',
-  API_ENDPOINT: 'http://localhost:8000/api',
-  
-}
-
+  API_ENDPOINT: 'https://obscure-journey-16541.herokuapp.com/api',
+  TOKEN_KEY: process.env.TOKEN_KEY || 'auth'
+};
